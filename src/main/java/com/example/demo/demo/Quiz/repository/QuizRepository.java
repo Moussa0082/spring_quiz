92e464741e200788447a0a670f0a83cbb1e0f3d6
unknown <bane8252@gmail.com>
2023-07-25 11:04:32 +0000

    deuxieme commit mardi

--- src/main/java/com/example/demo/demo/Quiz/repository/QuizRepository.java
+++ src/main/java/com/example/demo/demo/Quiz/repository/QuizRepository.java
@@ -4,6 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.demo.demo.Quiz.model.Quiz;
 
-public interface QuizRepository extends JpaRepository<Quiz, Long>{
+public interface QuizRepository extends JpaRepository<Quiz, Integer>{
     
 }
